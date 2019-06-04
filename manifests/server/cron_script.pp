af8e65532f0e1d63fb056628ed2ac0aa8d9febb6
Grant Davies <grant@veepshosting.com>
2019-06-04 16:01:00 +1000

    cron_script.pp

--- manifests/server/cron_script.pp
+++ manifests/server/cron_script.pp
@@ -1,5 +1,15 @@
 class rsnapshot::server::cron_script (
-  $script_path = $rsnapshot::params::script_path,
+  $backup_hourly_cron  = $rsnapshot::params::backup_hourly_cron,
+  $backup_time_minute  = $rsnapshot::params::backup_time_minute,
+  $backup_time_hour    = $rsnapshot::params::backup_time_hour,
+  $backup_time_weekday = $rsnapshot::params::backup_time_weekday,
+  $backup_time_dom     = $rsnapshot::params::backup_time_dom,
+  $retain_hourly       = $rsnapshot::params::retain_hourly,
+  $retain_daily        = $rsnapshot::params::retain_daily,
+  $retain_weekly       = $rsnapshot::params::retain_weekly,
+  $retain_monthly      = $rsnapshot::params::retain_monthly,
+  $script_path         = $rsnapshot::params::script_path,
+  $server_user         = $rsnapshot::params::server_user,
 ){
 
   file { $script_path:
@@ -9,56 +19,56 @@ class rsnapshot::server::cron_script (
     owner  => root,
   }
 
-file { "$script_path/rsnapshot_backup.sh":
-  ensure  => present,
-  group   => root,
-  mode    => '0544',
-  owner   => root,
-  require => File[$script_path],
-  source  => 'puppet:///modules/rsnapshot/rsnapshot_backup.sh',
-}
+  file { "$script_path/rsnapshot_backup.sh":
+    ensure  => present,
+    group   => root,
+    mode    => '0544',
+    owner   => root,
+    require => File[$script_path],
+    source  => 'puppet:///modules/rsnapshot/rsnapshot_backup.sh',
+  }
 
-  # cronjobs
+  ## CRON Jobs
 
   ## hourly
-#  if ($retain_hourly > 0) {
-#    cron { "rsnapshot-${name}-hourly" :
-#      command => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} hourly",
-#      user    => $server_user,
-#      hour    => $backup_hourly_cron,
-#      minute  => $backup_time_minute
-#    }
-#  }
+  if ($retain_hourly > 0) {
+    cron { rsnapshot-hourly :
+      command => '/etc/rsnapshot/scripts/rsnapshot_backup.sh hourly',
+      user    => $server_user,
+      hour    => $backup_hourly_cron,
+      minute  => $backup_time_minute
+    }
+  }
 
   ## daily
-#  if ($retain_daily > 0) {
-#    cron { "rsnapshot-${name}-daily" :
-#      command => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} daily",
-#      user    => $server_user,
-#      hour    => $backup_time_hour,
-#      minute  => ($backup_time_minute + 50) % 60
-#    }
-#  }
+  if ($retain_daily > 0) {
+    cron { rsnapshot-daily :
+      command => '/etc/rsnapshot/scripts/rsnapshot_backup.sh daily',
+      user    => $server_user,
+      hour    => $backup_time_hour,
+      minute  => $backup_time_minute
+    }
+  }
 
   ## weekly
-#  if ($retain_weekly > 0) {
-#    cron { "rsnapshot-${name}-weekly" :
-#      command => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} weekly",
-#      user    => $server_user,
-#      hour    => ($backup_time_hour + 3) % 24,
-#      minute  => ($backup_time_minute + 50) % 60,
-#      weekday => $backup_time_weekday
-#    }
-#  }
+  if ($retain_weekly > 0) {
+    cron { rsnapshot-weekly :
+      command => '/etc/rsnapshot/scripts/rsnapshot_backup.sh weekly',
+      user    => $server_user,
+      hour    => ($backup_time_hour + 3) % 24,
+      minute  => ($backup_time_minute + 50) % 60,
+      weekday => $backup_time_weekday
+    }
+  }
 
   ## monthly
-#  if ($retain_monthly > 0) {
-#    cron { "rsnapshot-${name}-monthly" :
-#      command  => "${rsnapshot::server::cmd_rsnapshot} -c ${config_file} monthly",
-#      user     => $server_user,
-#      hour     => ($backup_time_hour + 7) % 24,
-#      minute   => ($backup_time_minute + 50) % 60,
-#      monthday => $backup_time_dom
-#    }
-#  }
+  if ($retain_monthly > 0) {
+    cron { "rsnapshot-monthly :
+      command  => '/etc/rsnapshot/scripts/rsnapshot_backup.sh monthly',
+      user     => $server_user,
+      hour     => ($backup_time_hour + 7) % 24,
+      minute   => ($backup_time_minute + 50) % 60,
+      monthday => $backup_time_dom
+    }
+  }
 }
